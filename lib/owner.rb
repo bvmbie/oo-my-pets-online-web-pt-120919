01270993c27a575313e8af97466389c2c35eff5a
Henna Vahatiitto <hnnamv@gmail.com>
2020-01-18 18:03:46 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -33,23 +33,18 @@ class Owner
     @@all.clear
   end
   
-  # owner should be able to buy and sell a pet, which will change the pet's mood
-  
   def say_species
     "I am a #{self.species}."
   end
   
-  def buy_cat(name)
+  def buy_cat(name, self)
     new_cat = Cat.new(name)
     new_cat.owner = self
   end
   
-  def buy_dog(name)
+  def buy_dog(name, self)
     Dog.new(name)
     new_dog.owner = self
   end
-  
-  
-  # An owner should have the ability to walk all their dogs or feed all their cats, which will change the mood of all of their pets of that type to "happy"
   
 end
